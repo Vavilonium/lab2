4b8571a3a753abff001544d387ced0fa6f1f4c33
Vavilonium <89990312+Vavilonium@users.noreply.github.com>
2021-12-13 22:24:39 +0500

    зав-сть памяти от кол-ва элементов в векторе
    Вектор при добавлении в него элемента резервирует столько ячеек сколько нужно для следующей степени двойки, то есть если в векторе появляется 5-ый элемент, то вектор резервирует места для 8-ми элементов, если появляется 9-ый элемент, то - для 16 элементов.

---
+++ VectorCapacity.cpp
@@ -0,0 +1,23 @@
+﻿#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    vector<int> a;
+    a.push_back(1);
+    int x1 = sizeof(int) * a.capacity();
+    a.push_back(1);
+    int x2 = sizeof(int) * a.capacity();
+    cout << "Вектор для нового элемента тиипа int выделяет " << x2 - x1 << " байт" << '\n';
+    vector<int> b;
+    for (int i = 0; i < 100; i++)
+    {
+        b.push_back(1);
+        cout << b.capacity() << '\n';
+    }
+}
+
